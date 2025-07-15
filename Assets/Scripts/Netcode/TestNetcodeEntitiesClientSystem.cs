36fd9bbbdf82fc5db86a8a29e213e517e5300b8a
Arthur Bucia <abucia@z3r3p5.42lyon.fr>
2025-07-15 11:16:57 +0200

    add testCodeClient

--- Assets/Scripts/Netcode/TestNetcodeEntitiesClientSystem.cs
+++ Assets/Scripts/Netcode/TestNetcodeEntitiesClientSystem.cs
@@ -1,5 +1,7 @@
 using Unity.Burst;
 using Unity.Entities;
+using Unity.NetCode;
+using UnityEngine;
 
 [WorldSystemFilter(WorldSystemFilterFlags.ClientSimulation)]
 partial struct TestNetcodeEntitiesClientSystem : ISystem
@@ -13,7 +15,26 @@ partial struct TestNetcodeEntitiesClientSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        
+        bool hasInput = false;
+        foreach (var input in SystemAPI.Query<RefRO<InputMovementData>>())
+        {
+            if (input.ValueRO.Movement.x != 0 || input.ValueRO.Movement.y != 0)
+            {
+                hasInput = true;
+                break;
+            }
+        }
+
+        if (hasInput)
+        {
+            Entity rpcEntity = state.EntityManager.CreateEntity();
+            state.EntityManager.AddComponentData(rpcEntity, new SimpleRPC
+            {
+                value = 56
+            });
+            state.EntityManager.AddComponentData(rpcEntity, new SendRpcCommandRequest());
+            Debug.Log("RPC sent...");
+        }
     }
 
     [BurstCompile]
